2026fe6a2a432c8fe921eeb545740cca903bacbf
orlan <orlan@10.230.170.226>
2018-01-17 18:16:10 +0000

    Primer commit.

---
+++ src/kata5p1/KATA5P1.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata5p1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author orlando
+ */
+public class KATA5P1 {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.sql.SQLException
+     */
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\orlan\\Desktop\\KATA5.db"); 
+        
+        Statement statement = connection.createStatement(); 
+        ResultSet res = statement.executeQuery("SELECT * FROM PEOPLE;");
+        
+        
+        while (res.next()){
+            System.out.println("ID = " + res.getInt("Id"));
+            System.out.println("NAME =" + res.getString("Name"));
+            
+        }
+        
+        
+    }
+    
+}
